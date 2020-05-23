8e9dc80f93386909bf18ada582f1d5bf5ab4c3a2
Sergii <sergii.mykhailyk@gmail.com>
2020-05-23 21:44:02 +0300

    Updates

---
+++ precache-manifest.802395c36bc33c357e9e6750bbcf3950.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55dea662ed178e98e41963c5d7b94e7d",
+    "url": "/get_joke_app/index.html"
+  },
+  {
+    "revision": "45a1036d532efe975c9c",
+    "url": "/get_joke_app/static/css/main.e2fc19cc.chunk.css"
+  },
+  {
+    "revision": "1ce17c1265965c7f3dde",
+    "url": "/get_joke_app/static/js/2.a69ec8df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/get_joke_app/static/js/2.a69ec8df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a1036d532efe975c9c",
+    "url": "/get_joke_app/static/js/main.af3b875f.chunk.js"
+  },
+  {
+    "revision": "fb43e5c8d7fa690be64f",
+    "url": "/get_joke_app/static/js/runtime-main.27ac9742.js"
+  }
+]);
